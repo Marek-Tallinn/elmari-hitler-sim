eb0572ff9bf05c7b0803e27a5ba2cb309d2698e6
Switch <marek.tallinn@voco.ee>
2025-11-14 19:29:04 +0200

    Delete Hitler sim 4.0 directory

--- Hitler sim 4.0/settings.py
+++
@@ -1,9 +0,0 @@
-class Settings:
-    def __init__(self):
-        self.screen_width = 1280
-        self.screen_height = 960
-        self.bg_colour = (0, 255, 255)
-        self.caption = 'Hitler SIM'
-        self.bonus_score = 30
-        # See on saladus ja kui ma saan teada, et sa kasutasid seda ma saadan pomme su kordinaatidele.
-        self.infinite_gas_bombs = False
